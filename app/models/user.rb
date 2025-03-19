5e088753705dd2f273333c1f92e315d83fa1fd1a
kizailent <egami.t.ab@m.titech.ac.jp>
2025-03-19 21:29:27 +0000

    [Add] nameの一意性

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, presence: true, length: { maximum: 20, minimum: 2 }
+  validates :name, presence: true, length: { maximum: 20, minimum: 2 }, uniqueness: { case_sensitive: true }
   validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
